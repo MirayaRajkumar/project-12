b417569fc23d13fc09a2e81245f533c0a493a11b
MirayaRajkumar <87897710+MirayaRajkumar@users.noreply.github.com>
2021-07-30 21:39:46 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ function preload(){
   gardenImg = loadImage("garden.png");
   rabbitImg = loadImage("rabbit.png");
   appleImg = loadImage("apple.png");
+  leavesImg = loadImage("leaf.png");
 }
 
 function setup(){
@@ -29,10 +30,42 @@ function draw() {
   edges= createEdgeSprites();
   rabbit.collide(edges);
 
+  if(keyIsPressed(RIGHT_ARROW)){
+    rabbit.x = rabbit.x + 2;
+  }
+  if(keyIsPressed(LEFT_ARROW)){
+    rabbit.x = rabbit.x + -2;
+  }
+
   createApples();
+
+  createLeaves();
 
   drawSprites();
 }
+
+function createApples(){
+  if(frameCount % 80 == 0){
+    var apple = createSprite(100,25,20,20);
+    apple.addAnimation("apple",appleImg);
+    apple.scale = 0.05;
+    apple.velocityY = 2;
+    if(apple.x > 350){
+      apple.visible = false;
+    }
+  }
+}
+
+function createLeaves(){
+  if(frameCount % 100 == 0){
+    var leaves = createSprite(250,30,20,20);
+    leaves.addAnimation("leaves",leavesImg);
+    leaves.scale = 0.05;
+    leaves.velocityY = 1.5;
+
+  }
+}
+
 
 function createApples(){
   if(frameCount % 80 == 0){
